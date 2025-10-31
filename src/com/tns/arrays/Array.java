156e1ac2c737223031eff010b79e0268677ccf95
Kiran S <Kiran S@LAPTOP-KBTS72RF>
2025-11-01 00:58:38 +0530

    Commited

---
+++ src/com/tns/arrays/Array.java
@@ -0,0 +1,15 @@
+package com.tns.arrays;
+
+public class Array {
+	public static void main(String[] args) {
+		int arr1[]= {100,20,300,40,50};
+		
+		//Arrays.sort(arr1);
+		for(int i=0;i<arr1.length;i++)
+		{
+		System.out.println(arr1[i]);
+		}
+   
+	}
+
+}
